99b8f23bdcbdaad326b209c56188a1cd8fc8aad1
seohyeonKK <sts05158@gmail.com>
2021-01-20 18:44:37 +0900

    완주하지 못한 선수 김서현

---
+++ unfinished_athlete/seohyeonKim_vector.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+
+    string answer;
+
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    completion.push_back("");
+
+    for (int i = 0; i < completion.size(); i++) {
+        if (participant[i] != completion[i]) {
+            answer = participant[i];
+            break;
+        }
+    }
+
+    return answer;
+}
