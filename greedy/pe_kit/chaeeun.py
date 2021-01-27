f955b363f56cc1d8d7c44a50b2fb724d8bf5263b
cherry2030 <64332359+cherry2030@users.noreply.github.com>
2021-01-27 16:58:00 +0900

    체육복 박채은
    수정

--- greedy/pe_kit/chaeeun.py
+++ greedy/pe_kit/chaeeun.py
@@ -1,26 +1,14 @@
 def solution(n, lost, reserve):
-    cnt = 0
     # 5번 제한사항
     for i in reserve[:]:
         if i in lost:
             lost.remove(i)
             reserve.remove(i)
-    print(lost,reserve)
 
-    for i in lost:
-         if (cnt == len(reserve)): break
-         if (i+1) == reserve[cnt]:
-             cnt += 1
-         elif (i-1) == reserve[cnt]:
-             cnt += 1
+    for i in reserve[:]:
+        if (i-1) in lost:
+            lost.remove(i-1)
+        elif (i+1) in lost:
+            lost.remove(i+1)
 
-    return (n-len(lost)+cnt)
-
-
-n =10
-lost = [1,2,3,10]
-reserve = [3,5,9]
-# [1,2,4]/[3,7]
-
-a = solution(n, lost, reserve)
-print(a)
+    return (n-len(lost))
