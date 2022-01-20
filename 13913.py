11a3ffbcb2d667f6495dc278c8b80839e27b3dbb
Seunghyun Woo <seunghyunwoo99@gmail.com>
2022-01-20 11:51:37 +0900

    우승현

---
+++ 13913.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+N, K = map(int, input().split())
+
+q = deque([N])
+visited = [[-1, -1] for _ in range(100001)]
+visited[N][0] = 0
+path = []
+
+while q:
+    x = q.popleft()
+    if x == K:
+        print(visited[x][0])
+        while x != -1:
+            path.append(x)
+            x = visited[x][1]
+        print(*path[::-1])
+
+    for a in [x - 1, x + 1, 2 * x]:
+        if 0 <= a <= 100000:
+            if visited[a][0] == -1:
+                visited[a][0] = visited[x][0] + 1
+                visited[a][1] = x
+                q.append(a)
---
+++ 16953.py
@@ -0,0 +1,19 @@
+A, B = map(int, input().split())
+counts = []
+
+def dfs(current, count):
+    if current == B:
+        counts.append(count)
+        return
+    elif current > B:
+        return
+    else:
+        dfs(current*10 + 1, count+1)
+        dfs(current*2, count+1)
+
+dfs(A, 1)
+
+if counts:
+    print(min(counts))
+else:
+    print(-1)
